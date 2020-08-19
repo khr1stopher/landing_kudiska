d0e13af9e355405141473fc3b353ca1cd6667c06
khr1stopher <kkromans009@gmail.com>
2020-08-18 21:49:27 -0400

    estructura de inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesModule } from './pages/pages.module';
 
 const routes: Routes = [
-  
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path:'', loadChildren: () => import('./pages/inicio/inicio.module').then(mod => mod.InicioModule )}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/pages/inicio/inicio.module.ts
+++ src/app/pages/inicio/inicio.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from '../shared/shared.module';
+import { InicioComponent } from './inicio/inicio.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('./inicio/inicio.module').then(mod => mod.InicioModule)}
+  { path:'inicio', component: InicioComponent }
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [InicioComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     RouterModule
   ]
 })
-export class PagesModule { }
+export class InicioModule { }
